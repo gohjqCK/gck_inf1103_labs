6d1d451da6e2b7f609717744605373822a45a018
Choon Kee <2605698@sit.singaporetech.edu.sg>
2026-09-10 23:51:11 +0800

    Rudimentary Validation

--- auditor.py
+++ auditor.py
@@ -1,1 +1,17 @@
-print("test")
+inventory = 0
+while True:
+    try:
+        inventoryI = int(input("Enter inventory amount: "))
+
+        if str(inventory).isdigit() and inventoryI > 0:
+            if inventory >= 0:
+                inventory = inventory + inventoryI
+                print("Current inventory amount is: " + str(inventory))
+            elif inventory <= 0:
+                print("Please enter a valid number!")
+        else:
+            print("Please enter a valid number!")
+
+    except ValueError:
+        print("Please enter a valid number!")
+
