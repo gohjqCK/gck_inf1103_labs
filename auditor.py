839877282c67a4274fe5a82bdac3e8b35d378f00
Choon Kee <2605698@sit.singaporetech.edu.sg>
2026-09-11 00:03:33 +0800

    Added Overstock Alert

--- auditor.py
+++ auditor.py
@@ -4,13 +4,19 @@ while True:
         inventoryI = int(input("Enter inventory amount: "))
 
         if str(inventory).isdigit() and inventoryI > 0:
-            if inventory >= 0:
+            if inventory >= 0  and inventory < 500:
                 inventory = inventory + inventoryI
-                print("Current inventory amount is: " + str(inventory))
+                if inventory < 500:
+                    print("Current inventory amount is: " + str(inventory))
+                else:
+                    print("Current inventory amount is: " + str(inventory))
+                    print("ALERT! Inventory over 500! Ending program...")
+                    quit()
             elif inventory <= 0:
                 print("Please enter a valid number!")
+        
         else:
-            print("Please enter a valid number!")
+            print("Please enter a valid Number!")
 
     except ValueError:
         print("Please enter a valid number!")
