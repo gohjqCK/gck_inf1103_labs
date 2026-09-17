b1751e1ceb21fbbecbe25ba64e48781e3acf18ce
Choon Kee <2605698@sit.singaporetech.edu.sg>
2026-09-17 23:37:11 +0800

    added processes.py and defined 'get_valid_inut'

---
+++ processes.py
@@ -0,0 +1,15 @@
+def get_valid_input(drink):
+    print(drink)
+    return drink
+
+def process_delivery(drink):
+    print(drink)
+    return drink
+
+def calculate_tax(drink):
+    print(drink)
+    return drink
+
+def generate_report(drink):
+    print(drink)
+    return drink
