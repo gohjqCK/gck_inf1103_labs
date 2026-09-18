6aa3d3baa9b68ef4298574fe4c6b2368f371b99a
Choon Kee <2605698@sit.singaporetech.edu.sg>
2026-09-18 13:35:43 +0800

    updated auditor and added validator

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,22 +1,28 @@
+import processes
+
 inventory = 0
+total_value = 0
 while True:
     inventoryI = input("Enter inventory amount: ")
-
-    if inventoryI.lower() == "quit":
-        print("Quitting. Final inventory number is: " + str(inventory))
+    if processes.get_valid_input(inventoryI).lower() == "quit":
+        print("Quitting..")
         break
-    try:
-        if str(inventory).isdigit() and int(inventoryI) > 0:
-            if inventory >= 0  and inventory < 501:
-                inventory += int(inventoryI)
-                if inventory < 501:
-                    print("Current inventory amount is: " + str(inventory))
-                else:
-                    print("ALERT! Current inventory is: " + str(inventory) + "! " + str(inventory - 500) + " over the alllowed limit!")
-                    quit()
-            elif inventory <= 0:
-                print("Please enter a valid number!")
-        else:
-            print("Please enter a valid Number!")
-    except ValueError:
-        print("Please enter a valid number!")
+    else:
+        inventory = processes.get_valid_input(inventoryI)
+        processes.process_delivery(inventory, total_value)
+
+    # try:
+    #     if str(inventory).isdigit() and int(inventoryI) > 0:
+    #         if inventory >= 0  and inventory < 501:
+    #             inventory += int(inventoryI)
+    #             if inventory < 501:
+    #                 print("Current inventory amount is: " + str(inventory))
+    #             else:
+    #                 print("ALERT! Current inventory is: " + str(inventory) + "! " + str(inventory - 500) + " over the alllowed limit!")
+    #                 quit()
+    #         elif inventory <= 0:
+    #             print("Please enter a valid number!")
+    #     else:
+    #         print("Please enter a valid Number!")
+    # except ValueError:
+    #     print("Please enter a valid number!")
--- processes.py
+++ processes.py
@@ -1,15 +1,15 @@
-def get_valid_input(drink):
-    print(drink)
-    return drink
+def get_valid_input(inventoryI):
+    if inventoryI.isdigit() and int(inventoryI) > 0:
+        inventoryI = inventoryI
+    elif inventoryI.lower() == "quit":
+        inventoryI = "quit"
+    else:
+        inventoryI = "Please input a valid number"
+    return inventoryI
 
-def process_delivery(drink):
-    print(drink)
-    return drink
+def process_delivery(current_value, total_value):
+    total_value = current_value + total_value
+    return total_value
 
-def calculate_tax(drink):
-    print(drink)
-    return drink
-
-def generate_report(drink):
-    print(drink)
-    return drink
+def calculate_tax(amount):
+    print("hi")
