52b2e0ee0199cc0382abc8ca8912e61c92585523
Choon Kee <2605698@sit.singaporetech.edu.sg>
2026-09-23 22:28:35 +0800

    cleared legacy code on main

--- modular_auditor.py
+++ modular_auditor.py
@@ -24,19 +24,3 @@ while True:
         elif processes.get_valid_input(inventoryI) == "report":
             processes.generate_reports(total, fail)
             break
-
-    # try:
-    #     if str(inventory).isdigit() and int(inventoryI) > 0:
-    #         if inventory >= 0  and inventory < 501:
-    #             inventory += int(inventoryI)
-    #             if inventory < 501:
-    #                 print("Current inventory amount is: " + str(inventory))
-    #             else:
-    #                 print("ALERT! Current inventory is: " + str(inventory) + "! " + str(inventory - 500) + " over the alllowed limit!")
-    #                 quit()
-    #         elif inventory <= 0:
-    #             print("Please enter a valid number!")
-    #     else:
-    #         print("Please enter a valid Number!")
-    # except ValueError:
-    #     print("Please enter a valid number!")
